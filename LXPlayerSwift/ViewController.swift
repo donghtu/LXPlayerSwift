efc5ab9d040e54c94e96307625e4cfd67713dde5
donglingxiao <donglingxiao@sinochem.com>
2018-10-17 18:13:38 +0800

    初始化项目

---
+++ LXPlayerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LXPlayerSwift
+//
+//  Created by donglingxiao on 2018/10/17.
+//  Copyright © 2018年 donglingxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
